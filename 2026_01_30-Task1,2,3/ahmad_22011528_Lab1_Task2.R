5b03e25ba9918474ad9ee9f595d44c501bfad6c6
Ahmad Waffy Rez'Daniel Bin Ahmad Rizal <uxncode.dev@gmail.com>
2026-01-30 20:14:39 +0800

    Add files via upload

---
+++ 2026_01_30-Task1,2,3/ahmad_22011528_Lab1_Task2.R
@@ -0,0 +1,4 @@
+num1 <- 0.956786
+num2 <- 7.8345901
+print(round(num1, 2))
+print(round(num2, 3))
---
+++ 2026_01_30-Task1,2,3/ahmad_22011528_Lab1_Task3.R
@@ -0,0 +1,3 @@
+radius <- as.numeric(readline(prompt = "Enter the radius of the circle: "))
+area <- pi * radius^2
+print(area)
